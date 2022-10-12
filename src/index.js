9eae9f8554365ca13a8b4ed89c2bedd9ba464317
Marda <marda_da-capo@hotmail.com>
2022-10-12 17:28:17 -0500

    index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ const config = require("./config");
 
 const db = require("./utils/database");
 
+const productsRouter = require("./products/products.router");
+
+const initModels = require("./models/initModels");
+
 const app = express();
+
+db.authenticate()
+  .then(res => console.log("DB Authentication succesfully"))
+  .catch(err => console.log(err));
+
+db.sync()
+  .then(res => console.log("DB Synced"))
+  .catch(err => console.log(err));
+
+initModels();
 
 app.use(express.json());
 
@@ -13,6 +27,8 @@ app.get("/", (req, res) => {
   res.status(200).json({message: "OK!"});
 
 });
+
+app.use("/products", productsRouter);
 
 app.listen(config.port, () => {
 
